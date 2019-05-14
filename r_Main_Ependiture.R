e2f74096812cc8c883f99cdf03f340ceb8658d71
Kpavicic00 <kpavic00@fesb.hr>
2019-05-14 20:14:10 +0200

    expendetures commit

--- r_Main_Ependiture.R
+++ r_Main_Ependiture.R
@@ -113,5 +113,109 @@ g5 + theme(axis.title=element_text(size=17,face="bold",color = "gray16"),
 #the and relative average cost of a Serie A per player purchased
 
 
+#########################################
+# Seria A 
+seriaA <- liga_petice %>%
+  filter(Name_of_Legue == "SerieA")
+#########################################
+g_seriaA <- ggplot(seriaA,aes(x = Year, y = Expend_INFLACION , col = Name_of_Legue)) +
+  geom_line(alpha = 0.9) +
+  geom_smooth(lwd = 2, se = FALSE) +
+  scale_y_continuous("Average Expend  per player",labels = scales::comma) +
+  scale_colour_manual(values = c("darkolivegreen")) +
+  labs(title = "Average and Relative  Expenditure for Seria A  per player + INFLATION",color = " Names of Leagues\n") 
+
+g_seriaA + theme_tufte() + 
+  theme(
+    legend.position = c(0.7, 0.9),
+    legend.title = element_text(face = "bold", size = 12),)
+
+g_seriaA +theme( axis.title=element_text(size=17,face="bold"),
+            axis.text = element_text(face = "bold", size = 17,,color = "gray16"),
+            plot.title = element_text(size = 20, face = "italic",color = "gray16"))
+#####################################################################################################
+# PremierLeague 
+Premierleague <- liga_petice %>%
+  filter(Name_of_Legue == "PremierLeague")
+#########################################
+g_Premierleague <- ggplot(Premierleague,aes(x = Year, y = Expend_INFLACION , col = Name_of_Legue)) +
+  geom_line(alpha = 0.9) +
+  geom_smooth(lwd = 2, se = FALSE) +
+  scale_y_continuous("Average Expend  per player",labels = scales::comma) +
+  scale_colour_manual(values = c("cyan4")) +
+  labs(title = "Average and Relative  Expenditure for Premier League  per player + INFLATION",color = " Names of Leagues\n") 
+
+g_Premierleague + theme_tufte() + 
+  theme(
+    legend.position = c(0.7, 0.9),
+    legend.title = element_text(face = "bold", size = 12),)
+
+g_Premierleague +theme( axis.title=element_text(size=17,face="bold"),
+                 axis.text = element_text(face = "bold", size = 17,,color = "gray16"),
+                 plot.title = element_text(size = 20, face = "italic",color = "gray16"))
+#####################################################################################################
+# LaLiga
+Laliga <- liga_petice %>%
+  filter(Name_of_Legue == "LaLiga")
+#########################################
+g_Laliga <- ggplot(Laliga,aes(x = Year, y = Expend_INFLACION , col = Name_of_Legue)) +
+  geom_line(alpha = 0.9) +
+  geom_smooth(lwd = 2, se = FALSE) +
+  scale_y_continuous("Average Expend  per player",labels = scales::comma) +
+  scale_colour_manual(values = c("darkorange3")) +
+  labs(title = "Average and Relative  Expenditure for La Liga  per player + INFLATION",color = " Names of Leagues\n") 
+
+g_Laliga + theme_tufte() + 
+  theme(
+    legend.position = c(0.7, 0.9),
+    legend.title = element_text(face = "bold", size = 12),)
+
+g_Laliga +theme( axis.title=element_text(size=17,face="bold"),
+                        axis.text = element_text(face = "bold", size = 17,,color = "gray16"),
+                        plot.title = element_text(size = 20, face = "italic",color = "gray16"))
+#####################################################################################################
+# Bundesliga
+Bundesligaa <- liga_petice %>%
+  filter(Name_of_Legue == "Bundesliga")
+#########################################
+g_Bundesligaa <- ggplot(Bundesligaa,aes(x = Year, y = Expend_INFLACION , col = Name_of_Legue)) +
+  geom_line(alpha = 0.9) +
+  geom_smooth(lwd = 2, se = FALSE) +
+  scale_y_continuous("Average Expend  per player",labels = scales::comma) +
+  scale_colour_manual(values = c("darkred")) +
+  labs(title = "Average and Relative  Expenditure for Bundesliga  per player + INFLATION",color = " Names of Leagues\n") 
+
+g_Bundesligaa + theme_tufte() + 
+  theme(
+    legend.position = c(0.7, 0.9),
+    legend.title = element_text(face = "bold", size = 12),)
+
+g_Bundesligaa +theme( axis.title=element_text(size=17,face="bold"),
+                 axis.text = element_text(face = "bold", size = 17,,color = "gray16"),
+                 plot.title = element_text(size = 20, face = "italic",color = "gray16"))
+#####################################################################################################
+# Ligue1
+Ligue1a <- liga_petice %>%
+  filter(Name_of_Legue == "Ligue1")
+#########################################
 
 #####################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
